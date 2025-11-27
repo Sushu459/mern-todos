e7ed5c4b35921360776808cd4127a485ae0b2c11
T Sushmanth <tsushmanth@gmail.com>
2025-11-27 18:58:14 +0530

    updated axiosInstance file

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 // 🔐 Attach token to every request if available
